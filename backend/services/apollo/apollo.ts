4ae46360f8bce8724fba0d681c9e73080a612017
Dax Ravi <d@ironbay.co>
2022-01-14 10:15:49 -0500

    Split out things that should be extracted

--- backend/services/apollo/apollo.ts
+++ backend/services/apollo/apollo.ts
@@ -1,9 +1,3 @@
-import {
-  ApolloError,
-  ApolloServerBase,
-  runHttpQuery,
-} from "apollo-server-core";
-import { Headers } from "fetch-headers";
 import { typeDefs } from "./schema";
 import { useContext } from "@acme/core";
 import { merge } from "lodash-es";
@@ -16,62 +10,8 @@ import { UploadResolver } from "./resolvers/upload";
 
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 import { config } from "core/config";
-
-import type {
-  APIGatewayProxyEventV2,
-  APIGatewayProxyHandlerV2,
-  Context,
-} from "aws-lambda";
-
-type ApolloContext = {
-  event: APIGatewayProxyEventV2;
-  context: Context;
-};
-
-class ApolloServerLambda extends ApolloServerBase<ApolloContext> {
-  public createHandler() {
-    const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
-      await this.ensureStarted();
-
-      if (event.rawPath === "/" && event.requestContext.http.method === "GET") {
-        const landing = this.getLandingPage();
-        if (landing) {
-          return {
-            statusCode: 200,
-            headers: {
-              "Content-Type": "text/html",
-            },
-            body: landing.html,
-          };
-        }
-      }
-
-      const json = event.body
-        ? JSON.parse(event.body)
-        : event.queryStringParameters || {};
-      const result = await runHttpQuery([], {
-        query: json,
-        method: event.requestContext.http.method,
-        options: await this.graphQLServerOptions({
-          event: event,
-          context: context,
-        }),
-        request: {
-          method: event.requestContext.http.method,
-          url: event.requestContext.http.path,
-          headers: new Headers(event.headers),
-        },
-      });
-      return {
-        statusCode: 200,
-        headers: result.responseInit.headers,
-        body: result.graphqlResponse,
-      };
-    };
-
-    return handler;
-  }
-}
+import { ApolloError } from "apollo-server-core";
+import { ApolloServerLambda } from "./toBeExtracted";
 
 const verifier = CognitoJwtVerifier.create({
   userPoolId: config("COGNITO_USER_POOL_ID"),
