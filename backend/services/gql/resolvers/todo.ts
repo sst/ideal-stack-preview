730493d6317cdaaceea8d2803e3690bf4c9d7786
Dax Ravi <d@ironbay.co>
2022-01-19 14:23:06 -0500

    Sync

--- backend/services/gql/resolvers/todo.ts
+++ backend/services/gql/resolvers/todo.ts
@@ -1,5 +1,4 @@
 import { Todo } from "@acme/core";
-import { Config } from "@serverless-stack/backend";
 import { Resolvers } from "./types";
 
 export const TodoResolver: Resolvers = {
@@ -12,7 +11,6 @@ export const TodoResolver: Resolvers = {
   },
   User: {
     todos: async (parent, _args, ctx) => {
-      console.log(Config.MY_SPECIAL_CONFIG);
       const results = await Todo.forUser(ctx, { userId: parent.id! });
       return results.map((r) => ({
         id: r.id,
