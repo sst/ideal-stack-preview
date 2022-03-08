f2755da6061134399438a680b530d3518bc69238
Dax Ravi <d@ironbay.co>
2022-03-08 18:06:56 -0500

    Dynamic stacks

--- stacks/index.ts
+++ stacks/index.ts
@@ -3,7 +3,7 @@ import { Auth } from "./Auth";
 import { Database } from "./Database";
 import { Frontend } from "./Frontend";
 import { RemovalPolicy } from "aws-cdk-lib";
-import { init } from "./Functional";
+import { FunctionalStackProps, init, setStackProps } from "./Functional";
 import { GraphQL } from "./Graphql";
 import { Upload } from "./Upload";
 import { Parameter } from "./Parameter";
@@ -19,6 +19,18 @@ export default async function main(app: sst.App) {
   });
   if (app.local) app.setDefaultRemovalPolicy(RemovalPolicy.DESTROY);
 
-  await init(app, Database, Auth, Upload, GraphQL, Frontend);
+  await init(app, Database, Auth, Upload, GraphQL, Frontend, One, Two);
   Parameter.codegen();
 }
+
+// This is just a normal function that houses shared logic
+function CopyableStack(description: string) {
+  setStackProps({
+    description,
+  });
+}
+
+// These are the actual stacks
+const One = (_props: FunctionalStackProps) => CopyableStack("This is copy one");
+const Two = (_props: FunctionalStackProps) =>
+  CopyableStack("And this is copy two");
